72bb8dd82d14e23a7a788a8b2b1f21ef1c284662
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-07-13 00:26:04 -0700

    display all fields available in the sample data schema

--- components/DatabasePanel/DatabasePanelContainer.tsx
+++ components/DatabasePanel/DatabasePanelContainer.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import DraggableFieldTag from './DraggableFieldTag'
+import { paymentTableSchema } from '@/lib/store'
 
 export default function DatabasePanelContainer({className, ...props}: {className?: string}) {
   const [isOpen, setIsOpen] = React.useState(true)
@@ -7,8 +8,8 @@ export default function DatabasePanelContainer({className, ...props}: {className
     <div className='absolute bottom-0 min-w-full'>
       <button className='px-2 py-1 bg-white border-t border-x border-gray-200' onClick={() => setIsOpen(!isOpen)}>{isOpen ? 'Close' : 'Open'}</button>
       <div className={`w-full h-[200px] overflow-y-scroll bg-white border-t border-gray-200 ${className} ${isOpen ? 'visible' : 'hidden'}`}>
-        <div className='flex flex-col gap-2 m-4'>
-          <DraggableFieldTag id='first_name' name='First name' />
+        <div className='flex flex-row flex-wrap gap-2 m-4'>
+          {Object.values(paymentTableSchema).map((key, index) => (<DraggableFieldTag key={index} id={key.id} name={key.name} />))}
         </div>
       </div>
     </div>
