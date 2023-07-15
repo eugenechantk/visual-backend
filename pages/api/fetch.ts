7b892dc8dacc8cde06ee975724da5d8640f48b28
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-07-15 02:24:19 -0700

    added try catch block for fetch component data api

--- pages/api/fetch.ts
+++ pages/api/fetch.ts
@@ -6,12 +6,17 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { table } = req.query;
-  const prisma = getPrismaInstance();
-  if (table) {
-    // @ts-ignore
-    const results = await prisma[table! as keyof typeof prisma].findMany();
-    console.log('fetch result: ', results)
-    res.status(200).json({ results });
+  try {
+    const { table } = req.query;
+    const prisma = getPrismaInstance();
+    if (table !== "") {
+      // @ts-ignore
+      const results = await prisma[table! as keyof typeof prisma].findMany();
+      console.log("fetch result: ", results);
+      res.status(200).json({ results });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
   }
 }
